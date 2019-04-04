da3aaf10ba03f425f03d09ba05d43ee1a3757025
Phillipe <phillipe@bring.com.br>
2019-04-04 14:42:15 -0300

    Requisições via AJAX

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,40 @@
 ( function ( $ ) {
-    "use strict";
+  "use strict";
+
+//AJAX
+Array.from(document.querySelectorAll("#main-menu a"))
+.forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    var main = document.querySelector('main');
+    e.preventDefault();
+
+    if(!e.currentTarget.href.match(/#/)){
+      while(main.childElementCount){
+        main.removeChild(document.querySelector('main').firstElementChild);
+      }
+
+      var xhttp = new XMLHttpRequest();
+      var parser = new DOMParser();
+
+      xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+          let doc = parser.parseFromString(this.responseText,'text/html');
+
+          let kids = Array.from(doc.querySelector("#right-panel").children)
+                    .filter(element => !element.matches('header'));
+
+          kids.forEach(k =>
+            main.appendChild(k)
+          );
+        }
+      };
+
+      xhttp.open('GET',e.currentTarget.href,true);
+      xhttp.send();
+    }
+  });
+});
 
 
 // const brandPrimary = '#20a8d8'
@@ -21,54 +56,54 @@ function random (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-    var elements = 27
-    var data1 = []
-    var data2 = []
-    var data3 = []
+var elements = 27
+var data1 = []
+var data2 = []
+var data3 = []
 
-    for (var i = 0; i <= elements; i++) {
-      data1.push(random(50, 200))
-      data2.push(random(80, 100))
-      data3.push(65)
-    }
+for (var i = 0; i <= elements; i++) {
+  data1.push(random(50, 200))
+  data2.push(random(80, 100))
+  data3.push(65)
+}
 
 
     //Traffic Chart
     var ctx = document.getElementById( "trafficChart" );
     //ctx.height = 200;
     var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S'],
-            datasets: [
-            {
-              label: 'My First dataset',
-              backgroundColor: convertHex(brandInfo, 10),
-              borderColor: brandInfo,
-              pointHoverBackgroundColor: '#fff',
-              borderWidth: 2,
-              data: data1
-          },
-          {
-              label: 'My Second dataset',
-              backgroundColor: 'transparent',
-              borderColor: brandSuccess,
-              pointHoverBackgroundColor: '#fff',
-              borderWidth: 2,
-              data: data2
-          },
-          {
-              label: 'My Third dataset',
-              backgroundColor: 'transparent',
-              borderColor: brandDanger,
-              pointHoverBackgroundColor: '#fff',
-              borderWidth: 1,
-              borderDash: [8, 5],
-              data: data3
-          }
-          ]
+      type: 'line',
+      data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: convertHex(brandInfo, 10),
+          borderColor: brandInfo,
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 2,
+          data: data1
         },
-        options: {
+        {
+          label: 'My Second dataset',
+          backgroundColor: 'transparent',
+          borderColor: brandSuccess,
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 2,
+          data: data2
+        },
+        {
+          label: 'My Third dataset',
+          backgroundColor: 'transparent',
+          borderColor: brandDanger,
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 1,
+          borderDash: [8, 5],
+          data: data3
+        }
+        ]
+      },
+      options: {
             //   maintainAspectRatio: true,
             //   legend: {
             //     display: false
@@ -87,39 +122,39 @@ function random (min, max) {
 
             maintainAspectRatio: true,
             legend: {
-                display: false
+              display: false
             },
             responsive: true,
             scales: {
-                xAxes: [{
-                  gridLines: {
-                    drawOnChartArea: false
-                  }
-                }],
-                yAxes: [ {
-                      ticks: {
-                        beginAtZero: true,
-                        maxTicksLimit: 5,
-                        stepSize: Math.ceil(250 / 5),
-                        max: 250
-                      },
-                      gridLines: {
-                        display: true
-                      }
-                } ]
+              xAxes: [{
+                gridLines: {
+                  drawOnChartArea: false
+                }
+              }],
+              yAxes: [ {
+                ticks: {
+                  beginAtZero: true,
+                  maxTicksLimit: 5,
+                  stepSize: Math.ceil(250 / 5),
+                  max: 250
+                },
+                gridLines: {
+                  display: true
+                }
+              } ]
             },
             elements: {
-                point: {
-                  radius: 0,
-                  hitRadius: 10,
-                  hoverRadius: 4,
-                  hoverBorderWidth: 3
+              point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+                hoverBorderWidth: 3
               }
+            }
+
+
           }
+        } );
 
 
-        }
-    } );
-
-
-} )( jQuery );
+  } )( jQuery );
